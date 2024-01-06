d0070959b394bee2e4ee03e9a251952ecae9bd7f
Aryan Panja <136119675+aryan-panja@users.noreply.github.com>
2024-01-06 18:58:00 +0530

    Update settings.py

--- recipe/settings.py
+++ recipe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(8_6vptg+h1w!imf=abzft!_g+lkf-)j@155@!0bxwdmc(4$t'
+SECRET_KEY = 'NOPE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
